419bff1b23d500d51c78fc24867fea4ecb48e96e
arun-arunisto <arun.arunisto2@gmail.com>
2023-06-07 15:09:52 +0530

    second Commit

--- userAuthentication/user_blueprint.py
+++ userAuthentication/user_blueprint.py
@@ -21,24 +21,32 @@ def logout():
 @user_bp.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    #print(form.is_submitted(), form.validate_on_submit())
-    if form.is_submitted():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password1.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("user_bp.login"))
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        try:
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password1.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("user_bp.login"))
+        except Exception as e:
+            print(e)
+            flash("Some Error Occured")
+            return redirect(url_for("user_bp.register"))
     return render_template("register.html", form=form)
 
 @user_bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if form.is_submitted():
-        user = User.query.filter_by(email = form.email.data).first()
-        if user is not None and user.check_password(form.password.data):
-            login_user(user)
-            return redirect(url_for('user_bp.home'))
-        flash("Invalid email or password")
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email = form.email.data).first()
+            if user is not None and user.check_password(form.password.data):
+                login_user(user)
+                return redirect(url_for('user_bp.home'))
+            flash("Invalid email or password")
+        except Exception as e:
+            return redirect(url_for('user_bp.login'))
     return render_template("login.html", form=form)
 
 
